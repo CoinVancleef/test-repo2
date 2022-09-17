1071432aa79f20ed12bc109c7e8718ce764d5fc5
Arsenii Tsubera <111arsen111@gmail.com>
2022-09-17 14:49:58 +0300

    created data.ts

---
+++ data.ts
@@ -0,0 +1,19 @@
+const data: {
+  name: string;
+  age: number;
+  email: string;
+  isLoggedIn: boolean;
+}[] = [
+  {
+    name: "Joe Schmoe",
+    age: 23,
+    email: "joe@schmoe.com",
+    isLoggedIn: true,
+  },
+  {
+    name: "Alice Margatroid",
+    age: 20,
+    email: "alicemarga@ukr.net",
+    isLoggedIn: false,
+  },
+];
